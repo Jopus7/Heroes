93b91d192bc494b1c1feaa8383e2ac492ad335c8
twój Jopus7 <micha.jopa@gmail.com>
2023-05-29 12:16:38 +0200

    HE-002_base_code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,49 @@
-import './styles/main.scss'
+import './styles/main.scss';
+import { showMessage } from './lib/dom';
 
+async function bake() {
+    const title = 'Baking cookies';
+    let counter = 0;
 
-console.log('ala')
+    console.clear();
+    console.group(title);
+
+    counter++;
+    console.log(`${counter} - Add ingredients`);
+    showMessage(`${counter} - Add ingredients`, title);
+
+    counter++;
+    console.log(`${counter} - Mix ingredients`);
+    showMessage(`${counter} - Mix ingredients`, title, true);
+
+    function bakeInOven(cb: () => void) {
+        setTimeout(() => {
+            counter++;
+            console.log(`${counter} - Bake at 200 degrees for 10 minutes`);
+            showMessage(
+                `${counter} -Bake at 200 degrees for 10 minutes`,
+                title,
+                true,
+            );
+            cb();
+        }, 3000);
+    }
+    bakeInOven(eat);
+    function eat() {
+        counter++;
+        console.log(`${counter} - Eat cookies`);
+        showMessage(`${counter} - Eat cookies`, title, true);
+
+        console.groupEnd();
+    }
+
+    counter++;
+    console.log(`${counter} - Eat cookies`);
+    showMessage(`${counter} - Eat cookies`, title, true);
+
+    console.groupEnd();
+}
+// @ts-ignore
+document.querySelector('#bake-cookies').addEventListener('click', async () => {
+    bake();
+});
